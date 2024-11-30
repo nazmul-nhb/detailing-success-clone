a2e97e6e1a6ce2e695cac6c473ccfde0d9401072
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-11-30 21:23:26 +0600

    setup tailwindcss and favicon

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{ts,js,jsx,tsx,html}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#121212',
+
+      }
+    },
+  },
+  plugins: [],
+}
