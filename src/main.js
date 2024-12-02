a6679702b453e9e3fa7d6361b25db7509c8988ef
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-12-02 22:08:44 +0600

    created basic dummy navbar

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 import './style.css';
+
+
+document.querySelector('#mobile-menu-button').addEventListener('click', () => {
+    const mobileMenu = document.querySelector('#mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+});
